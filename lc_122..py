2f9cd8f5478ce257fc715f76c1186a075c9b1857
Melvin M T <melvinthankachan101@gmail.com>
2024-07-07 19:20:52 +0530

    152. Maximum Product Sub-array

--- lc_122..py
+++ lc_122..py
@@ -2,21 +2,16 @@ class Solution(object):
     def maxProfit(self, prices):
         no_of_prices = len(prices)
         max_profit = 0
-
         for i in range(no_of_prices - 1):
             if prices[i] < prices[i + 1]:
                 max_profit += prices[i + 1] - prices[i]
-
         return max_profit
 
 
 result = Solution()
-prices = [1,2,3,4,5]
+prices = [1, 2, 3, 4, 5]
 output = result.maxProfit(prices)
-
 print(output)
-
 expected = 4
-
 if output == expected:
     print("jayichu mone!")
---
+++ lc_152.py
@@ -0,0 +1,23 @@
+# 152. Maximum Product Sub-array
+
+
+class Solution(object):
+    def maxProduct(self, nums):
+        result = nums[0]
+        n = len(nums)
+        for i in range(n):
+            mul = nums[i]
+            for j in range(i + 1, n):
+                mul *= nums[j]
+                if mul > result:
+                    result = mul
+        return result
+
+
+result = Solution()
+nums = [-2,0,-1]
+output = result.maxProduct(nums)
+print(output)
+expected = 0
+if output == expected:
+    print("jayichu mone!")

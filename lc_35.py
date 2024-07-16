d771d9d061e1fd4567a8b6ce4bae01ea04fa3f44
Melvin M T <melvinthankachan101@gmail.com>
2024-07-16 15:00:10 +0530

    Daily Leetcode - Problem number 35

---
+++ lc_35.py
@@ -0,0 +1,42 @@
+# 35. Search Insert Position
+
+
+# Linear Search
+class Solution(object):
+    def searchInsert(self, nums, target):
+        index = 0
+        for i, num in enumerate(nums):
+            if num == target:
+                return i
+            elif num < target:
+                index += 1
+            else:
+                break
+        return index
+
+
+# Binary Search
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left = 0
+        right = len(nums) -1
+        while left <= right:
+            mid = (left + right)
+            mid_element = nums[mid]
+            if mid_element == target:
+                return mid
+            elif mid_element > target:
+                right = mid - 1
+            elif mid_element < target:
+                left = mid + 1
+        return left
+
+
+result = Solution()
+nums = [1, 3, 5, 6]
+target = 3
+output = result.searchInsert(nums, target)
+print(output)
+expected_output = 1
+if output == expected_output:
+    print("Jayichu mwone!")

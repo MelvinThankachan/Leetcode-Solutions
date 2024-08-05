7a7d028fcbaeb7e155a824e45eddee6b199f3ffb
Melvin M T <melvinthankachan101@gmail.com>
2024-08-05 09:28:53 +0530

    Daily leetcode - Problems: 1508, 2503

---
+++ lc_1508.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def rangeSum(self, nums: List[int], n: int, left: int, right: int) -> int:
+        MODULO = 10**9 + 7
+        sums = []
+        for i in range(n):
+            for j in range(i + 1, n + 1):
+                sums.append(sum(nums[i:j]) % MODULO)
+        sums.sort()
+        return sum(sums[left - 1 : right]) % MODULO
+
+
+result = Solution()
+nums = [1, 2, 3, 4]
+n = 4
+left = 1
+right = 5
+output = result.rangeSum(nums, n, left, right)
+print("output", output)
+expected = 13
+if output == expected:
+    print("jayichu mone!")
---
+++ lc_2503.py
@@ -0,0 +1,43 @@
+# 2053. Kth Distinct String in an Array
+
+from typing import List
+from collections import Counter
+
+
+class Solution:
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        strings_map = {}
+        for string in arr:
+            if string not in strings_map:
+                strings_map[string] = 1
+            else:
+                strings_map[string] += 1
+
+        distinct_strings = []
+        for string in strings_map.keys():
+            if strings_map[string] == 1:
+                distinct_strings.append(string)
+
+        result = ""
+        if k <= len(distinct_strings):
+            result += distinct_strings[k - 1]
+        return result
+
+    def kthDistinct(self, arr: List[str], k: int) -> str:
+        strings_map = Counter(arr)
+        for i in arr:
+            if strings_map[i] == 1:
+                k -= 1
+            if k == 0:
+                return i
+        return ""
+
+
+result = Solution()
+arr = ["d", "b", "c", "b", "c", "a"]
+k = 2
+output = result.kthDistinct(arr, k)
+print("output", output)
+expected = "a"
+if output == expected:
+    print("jayichu mone!")

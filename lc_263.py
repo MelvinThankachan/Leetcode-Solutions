e5356baaf4d81a95be0c2d76ca10a4af62d7b091
Melvin M T <melvinthankachan101@gmail.com>
2024-08-19 21:06:59 +0530

    Ugly number - I update

--- lc_263.py
+++ lc_263.py
@@ -3,33 +3,14 @@ from math import sqrt, ceil
 
 class Solution:
     def isUgly(self, n: int) -> bool:
-        # if n <= 0:
-        #     return False
-
-        # while n % 2 == 0 or n % 3 == 0 or n % 5 == 0:
-        #     if n % 2 == 0:
-        #         n /= 2
-        #     if n % 3 == 0:
-        #         n /= 3
-        #     if n % 5 == 0:
-        #         n /= 5
-
-        # return n == 1
-
-
-        if n % 2 == 0:
-            n /= 2
-        if n % 3 == 0:
-            n /= 3
-        if n % 5 == 0:
-            n /= 5
-            
-        if n == 1:
-            return True
-        else:
+        if n <= 0:
             return False
 
-        return self.isUgly(n)
+        for prime in [2, 3, 5]:
+            while n % prime == 0:
+                n /= prime
+
+        return n == 1
 
 
 result = Solution()

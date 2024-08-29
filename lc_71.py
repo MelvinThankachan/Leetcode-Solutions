f51b8d921c1b2e51667fe734da7100966337e17a
Melvin M T <melvinthankachan101@gmail.com>
2024-08-29 09:28:16 +0530

    Daily LeetCode - Problem: 71

---
+++ lc_71.py
@@ -0,0 +1,26 @@
+# 71. Simplify Path
+
+
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        files = path.split("/")
+        for file_name in files:
+            if file_name == "..":
+                if stack:
+                    stack.pop()
+            elif file_name and file_name != ".":
+                stack.append(file_name)
+
+        return "/" + "/".join(stack)
+
+
+result = Solution()
+path = "/.../a/../b/c/../d/./"
+output = result.simplifyPath(path)
+print("output", output)
+expected = "/.../b/d"
+if output == expected:
+    print("Test passed successfully!")
+else:
+    print("Test failed!")
---
+++ lc_947.py
@@ -0,0 +1,33 @@
+# 947. Most Stones Removed with Same Row or Column
+
+
+from typing import List
+from collections import defaultdict
+
+
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        row_map = defaultdict(int)
+        col_map = defaultdict(int)
+        removed_stones = 0
+        for row, col in stones:
+            row_map[row] += 1
+            col_map[col] += 1
+
+        while len(stones) > 1:
+            (row, col) = stones.pop()
+            if row_map[row] > 1 or col_map[col] > 1:
+                removed_stones += 1
+
+        return removed_stones
+
+
+result = Solution()
+stones = [[3, 2], [3, 1], [4, 4], [1, 1], [0, 2], [4, 0]]
+output = result.removeStones(stones)
+print("output", output)
+expected = 4
+if output == expected:
+    print("Test passed successfully!")
+else:
+    print("Test failed!")
